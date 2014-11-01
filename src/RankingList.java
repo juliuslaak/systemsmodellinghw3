86c41850457ca85babff97740b383b3542e5d9b0
Janno Siim <jannosiim@gmail.com>
2014-11-01 22:23:37 +0200

    added restaurant stub and some modification for restaurant class in the model as well.

--- src/RankingList.java
+++ src/RankingList.java
@@ -3,7 +3,6 @@ public class RankingList {
    
 	
 	
-	
    @Override
    public String toString() {
 	// TODO Auto-generated method stub
---
+++ src/Restaurant.java
@@ -0,0 +1,83 @@
+/**
+ * @(#) Restaurant.java
+ */
+
+public class Restaurant
+{
+	public String name;
+	
+	public int reputation;
+	
+	public float budget;
+	
+	public String address;
+	
+	public String city;
+	
+	private java.util.List<Table> tables;
+	
+	private Barman barman;
+	
+	private java.util.List<Waiter> waiters;
+	
+	private Chef chef;
+	
+	private java.util.List foodItems;
+	
+	public void payMonthlyCosts( )
+	{
+		
+	}
+	
+	private void initDishes( )
+	{
+		
+	}
+	
+	private void initBeverages( )
+	{
+		
+	}
+	
+	public void initMenuItemsCorrespondingPrices( )
+	{
+		
+	}
+	
+	public void rateService( )
+	{
+		
+	}
+	
+	public int calculateReputation( )
+	{
+		return 0;
+	}
+	
+	public float receivePaymentForOrder( )
+	{
+		return 0;
+	}
+	
+	public void assignTablesToWaiters( )
+	{
+		
+	}
+	
+	public boolean checkIfBudgetEnough( )
+	{
+		return false;
+	}
+	
+	public LevelOfExperience checkExperience( )
+	{
+		return null;
+	}
+	
+	public float paySuppliers( )
+	{
+		return 0;
+	}
+	
+	
+}
